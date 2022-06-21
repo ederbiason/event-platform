4ad0aef64aa21896ac5d44f1b0ece037ed9a4688
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-06-21 19:35:41 -0300

    components
    Create all the components of the project and finished the header and font

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
-import { gql, useQuery } from "@apollo/client"
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Header } from "./components/Header"
+import { Event } from "./pages/Event"
 
 function App() {
-  const {data} = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
-
   return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    <div>
+      <Header />
+
+      <Event />
+    </div>
   )
 }
 
---
+++ src/pages/Event.tsx
@@ -0,0 +1,7 @@
+export function Event() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
