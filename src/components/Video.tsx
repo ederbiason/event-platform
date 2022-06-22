4448de767c378754280822b0c3e3848aa169ae9d
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-06-22 20:42:12 -0300

    router and fetch datas
    Added react-router-dom to change the lesson when the client click the lesson and atch the slug and put in the url
    
    Using Apollo Client to do the fetch

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,15 +1,65 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
-import { CaretRight, DiscordLogo, FileArrowDown, Image, Lightning } from "phosphor-react";
+import { gql, useQuery } from "@apollo/client";
+import { CaretRight, CircleNotch, DiscordLogo, FileArrowDown, Image, Lightning } from "phosphor-react";
 
 import '@vime/core/themes/default.css';
 
-export function Video() {
+const GET_LESSON_BY_SLUG = gql`
+    query GetLessonBySlug ($slug: String) {
+        lesson(where: {slug: $slug}) {
+            title
+            videoId
+            description
+            teacher {
+                name
+                bio
+                avatarURL
+            }
+        }
+    }
+`
+
+interface GetLessonBySlugResponse {
+    lesson: {
+        title: string;
+        videoId: string;
+        description: string;
+        teacher: {
+            bio: string;
+            avatarURL: string;
+            name: string;
+        }
+    }
+}
+
+interface VideoProps {
+    lessonSlug: string;
+}
+
+export function Video(props: VideoProps) {
+    const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG, {
+        variables: {
+            slug: props.lessonSlug
+        }
+    })
+
+    if (!data) {
+        return (
+            <div className="flex flex-1 items-center justify-center">
+                <div className="flex gap-2">
+                    <CircleNotch size={40} className="animate-spin"/>
+                    <p className="text-4xl">Carregando...</p>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div className="flex-1">
             <div className="bg-black flex justify-center">
                 <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
                     <Player>
-                        <Youtube videoId="SO4-izct7Mc"/>
+                        <Youtube videoId={data.lesson.videoId}/>
                         <DefaultUi />
                     </Player>
                 </div>
@@ -19,24 +69,25 @@ export function Video() {
                 <div className="flex items-start gap-16">
                     <div className="flex-1">
                         <h1 className="text-2xl font-bold">
-                            Aula 01 - Abertura do Ignite Lab
+                            {data.lesson.title}
                         </h1>
                         <p className="mt-4 text-gray-200 leading-relaxed">
-                            Participe da abertura do Ignite Lab e descubra na prática a biblioteca JavaScript utilizada por grandes empresas na construção de interfaces web. E vamos juntos, nessa semana, acelerar sua evolução na programação 🚀
+                            {data.lesson.description}
                         </p>
 
                         <div className="flex items-center gap-4 mt-6">
                             <img 
                                 className="h-16 w-16 rounded-full border-2 border-blue-500"
-                                src="https://github.com/ederbiason.png" alt="" 
+                                src={data.lesson.teacher.avatarURL} 
+                                alt={`${data.lesson.teacher.name} image`} 
                             />
 
                             <div className="leading-relaxed">
                                 <strong className="font-bold text-2xl block">
-                                    Eder Biason
+                                    {data.lesson.teacher.name}
                                 </strong>
                                 <span className="text-gray-200 text-sm block">
-                                    Front-End Developer Rocketseat
+                                    {data.lesson.teacher.bio}
                                 </span>
                             </div>
                         </div>
