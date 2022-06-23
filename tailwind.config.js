7c75e136ee787a93b38b0129514badd04fc0a9bc
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-06-23 19:57:22 -0300

    Subscribe page
    Created the subscribe page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        blur: 'url(/src/assets/images/blur-background.png)',
+      },
       fontFamily: {
         sans: 'Roboto, sans-serif',
       },
