4c1094a6426f4546ef052bae17b34d41e78a080b
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-06-24 19:44:55 -0300

    adding graphql codegen
    Added graphql code generator to not to worry about with the typing of the queries and mutation.
    
    He create a file with all the typescript and more things that prevent the code have errors

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,54 +1,26 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
-import { gql, useQuery } from "@apollo/client";
 import { CaretRight, CircleNotch, DiscordLogo, FileArrowDown, Image, Lightning } from "phosphor-react";
 import { Footer } from "./Footer";
 
 import '@vime/core/themes/default.css';
-
-const GET_LESSON_BY_SLUG = gql`
-    query GetLessonBySlug ($slug: String) {
-        lesson(where: {slug: $slug}) {
-            title
-            videoId
-            description
-            teacher {
-                name
-                bio
-                avatarURL
-            }
-        }
-    }
-`
-
-interface GetLessonBySlugResponse {
-    lesson: {
-        title: string;
-        videoId: string;
-        description: string;
-        teacher: {
-            bio: string;
-            avatarURL: string;
-            name: string;
-        }
-    }
-}
+import { useGetLessonBySlugQuery } from "../graphql/generated";
 
 interface VideoProps {
     lessonSlug: string;
 }
 
 export function Video(props: VideoProps) {
-    const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG, {
+    const { data } = useGetLessonBySlugQuery({
         variables: {
             slug: props.lessonSlug
         }
     })
 
-    if (!data) {
+    if (!data || !data.lesson) {
         return (
             <div className="flex flex-1 items-center justify-center">
                 <div className="flex gap-2">
-                    <CircleNotch size={40} className="animate-spin"/>
+                    <CircleNotch size={40} className="animate-spin" />
                     <p className="text-4xl">Carregando...</p>
                 </div>
             </div>
@@ -60,7 +32,7 @@ export function Video(props: VideoProps) {
             <div className="bg-black flex justify-center">
                 <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
                     <Player>
-                        <Youtube videoId={data.lesson.videoId} key={data.lesson.videoId}/>
+                        <Youtube videoId={data.lesson.videoId} key={data.lesson.videoId} />
                         <DefaultUi />
                     </Player>
                 </div>
@@ -76,32 +48,34 @@ export function Video(props: VideoProps) {
                             {data.lesson.description}
                         </p>
 
-                        <div className="flex items-center gap-4 mt-6">
-                            <img 
-                                className="h-16 w-16 rounded-full border-2 border-blue-500"
-                                src={data.lesson.teacher.avatarURL} 
-                                alt={`${data.lesson.teacher.name} image`} 
-                            />
+                        {data.lesson.teacher && (
+                            <div className="flex items-center gap-4 mt-6">
+                                <img
+                                    className="h-16 w-16 rounded-full border-2 border-blue-500"
+                                    src={data.lesson.teacher.avatarURL}
+                                    alt={`${data.lesson.teacher.name} image`}
+                                />
 
-                            <div className="leading-relaxed">
-                                <strong className="font-bold text-2xl block">
-                                    {data.lesson.teacher.name}
-                                </strong>
-                                <span className="text-gray-200 text-sm block">
-                                    {data.lesson.teacher.bio}
-                                </span>
+                                <div className="leading-relaxed">
+                                    <strong className="font-bold text-2xl block">
+                                        {data.lesson.teacher.name}
+                                    </strong>
+                                    <span className="text-gray-200 text-sm block">
+                                        {data.lesson.teacher.bio}
+                                    </span>
+                                </div>
                             </div>
-                        </div>
+                        )}
                     </div>
 
                     <div className="flex flex-col gap-4 mt-4">
                         <a href="https://discord.gg/UVZ8wHqZXt" target="_blank" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
-                            <DiscordLogo size={24}/>
+                            <DiscordLogo size={24} />
                             Comunidade do Discord
                         </a>
 
                         <a href="#" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
-                            <Lightning size={24}/>
+                            <Lightning size={24} />
                             Acesse o desafio
                         </a>
                     </div>
@@ -110,7 +84,7 @@ export function Video(props: VideoProps) {
                 <div className="gap-8 mt-20 grid lg:grid-cols-2 grid-cols-1">
                     <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
                         <div className="bg-green-700 h-full p-6 flex items-center">
-                            <FileArrowDown size={40}/>
+                            <FileArrowDown size={40} />
                         </div>
 
                         <div className="py-6 leading-relaxed">
@@ -123,13 +97,13 @@ export function Video(props: VideoProps) {
                         </div>
 
                         <div className="h-full p-6 lg:flex items-center hidden">
-                            <CaretRight size={24}/>
+                            <CaretRight size={24} />
                         </div>
                     </a>
 
                     <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
                         <div className="bg-green-700 h-full p-6 flex items-center">
-                            <Image size={40}/>
+                            <Image size={40} />
                         </div>
 
                         <div className="py-6 leading-relaxed">
@@ -142,7 +116,7 @@ export function Video(props: VideoProps) {
                         </div>
 
                         <div className="h-full p-6 lg:flex items-center hidden">
-                            <CaretRight size={24}/>
+                            <CaretRight size={24} />
                         </div>
                     </a>
                 </div>
