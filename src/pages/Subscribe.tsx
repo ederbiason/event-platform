63c8f0eb15d5e097fa0cb6dc388bc52c4c8d86cb
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-06-27 15:11:30 -0300

    Responsive subscribe page
    Adding some classes to made the subscribe page responsive

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -28,18 +28,20 @@ export function Subscribe() {
 
     return (
         <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-            <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+            <div className="w-full max-w-[1100px] flex lg:flex-row flex-col items-center justify-between lg:mt-20 mt-10 mx-auto">
                 <div className="max-w-[640px]">
-                    <Logo />
-                    <h1 className="mt-8 text-[2.5rem] leading-tight">
+                    <div className="flex lg:justify-start justify-center">
+                        <Logo />
+                    </div>
+                    <h1 className="mt-8 text-[2.5rem] leading-tight lg:text-left text-center lg:p-0 px-5">
                         Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React</strong>
                     </h1>
-                    <p className="mt-4 text-gray-200 leading-relaxed">
+                    <p className="mt-4 text-gray-200 leading-relaxed lg:text-left text-center lg:p-0 px-7">
                         Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
                     </p>
                 </div>
 
-                <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+                <div className="p-8 bg-gray-700 border border-gray-500 rounded lg:mt-0 mt-6">
                     <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
 
                     <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
