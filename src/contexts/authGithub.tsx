107a64dab7c319e0f8cf380aa5cde8a82b291f0d
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-07-19 17:10:33 -0300

    Context useEffect
    Created a useEffect to make sure that the user is logged using the local storage

--- src/contexts/authGithub.tsx
+++ src/contexts/authGithub.tsx
@@ -1,5 +1,5 @@
-import { createContext, ReactNode, useState } from "react";
-import { getAuth, signInWithPopup, GithubAuthProvider, UserCredential, User, IdTokenResult } from "firebase/auth";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { getAuth, signInWithPopup, GithubAuthProvider, User } from "firebase/auth";
 import { app } from '../services/firebase';
 
 const provider = new GithubAuthProvider()
@@ -18,8 +18,21 @@ export const AuthGithubContext = createContext({} as AuthContextType)
 
 export function AuthGithubProvider({ children }: AuthGithubProps) {
     const auth = getAuth(app)
-    const [user, setUser] = useState<User>()
+    const [user, setUser] = useState<User | string>()
     
+    useEffect(() => {
+        const loadStoreAuth = () => {
+            const sessionToken = sessionStorage.getItem("@AuthFirebase:token")
+            const sessionUser = sessionStorage.getItem("@AuthFirebase:user")
+
+            if(sessionToken && sessionUser) {
+                setUser(sessionUser)
+            }
+        }
+        loadStoreAuth()
+    }, [])
+    
+
     const signInWithGithub = () => {
         signInWithPopup(auth, provider)
             .then((result) => {
